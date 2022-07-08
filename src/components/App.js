ccece24a438db7263b4c0c6ecc26c65e5e3c3732
Luke Geneva <lukegeneva@gmail.com>
2022-07-07 22:56:40 -0500

    Change code styling
    Changed double quotes to single.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./app.css";
+import React from 'react';
+import './app.css';
 
 const App = () => {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   module: {
     rules: [
       {
@@ -11,22 +11,22 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: {
           // `.swcrc` can be used to configure swc
-          loader: "swc-loader",
+          loader: 'swc-loader',
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(jpe?g|gif|png|svg)/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public/index.html"),
+      template: path.join(__dirname, 'public/index.html'),
     }),
   ],
   devServer: {
